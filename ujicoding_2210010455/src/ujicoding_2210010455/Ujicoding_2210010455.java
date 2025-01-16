e387f49bcfce1cf3df3b2a3efa18e98349501fe9
alyaa12345 <alyanormaida123@gmail.com>
2025-01-16 15:02:25 +0800

    Add files via upload

---
+++ ujicoding_2210010455/src/ujicoding_2210010455/Ujicoding_2210010455.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ujicoding_2210010455;
+import javax.swing.JOptionPane;
+import sys.crudTable;
+import gui.framenilai;
+
+
+/**
+ *
+ * @author ALYA NORMAIDA
+ */
+public class Ujicoding_2210010455 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new framenilai().setVisible(true);
+    }
+    
+}
