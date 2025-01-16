50601ae3b1c32a36aad46faf2a873e605e296e60
alyaa12345 <alyanormaida123@gmail.com>
2025-01-16 15:01:01 +0800

    Add files via upload

---
+++ hukum/src/hukum/Hukum.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hukum;
+import javax.swing.JOptionPane;
+import sys.crudTable;
+import gui_.FrameUtama;
+
+/**
+ *
+ * @author ALYA NORMAIDA
+ */
+public class Hukum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrameUtama().setVisible(true);
+//        crudTable myObject = new crudTable();
+//        String nama[]={"id","nama_perkara","uud_perkara"};
+//        String isiTabel[]={"1","pencurian","uud1945"};
+//    
+//    if (myObject.DuplicateKey("perkara", "id", "KD02")){
+//        JOptionPane.showMessageDialog(null,"Data sudah ada");
+//    }else{
+//        myObject.SimpanDinamis("perkara", nama, isiTabel);
+//    }
+    
+    }
+}
